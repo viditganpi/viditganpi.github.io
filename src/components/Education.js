be1719db2712d5e9ac880356377e03aa10e9e51b
vidit mathur <viditganpi10@gmail.com>
2022-03-01 00:24:00 +0530

    Resolving accordion bug

--- src/components/Education.js
+++ src/components/Education.js
@@ -3,17 +3,28 @@ const data = require('../data/data.json');
 
 class Education extends React.Component{
     render(){
+        let count = 0;
         const eduList = data.education.map((edu) => {
+            const collapseClass = `collapse-${count}`;
+            const areaLabel = `heading-${count}`;
+            const collapseDivClass = count==0? "panel-collapse collapse in" : " panel-collapse collapse";
+            let eduTag = <a className="collapsed" data-toggle="collapse" data-parent="#accordion" href={`#${collapseClass}`} aria-expanded="false" aria-controls={collapseClass}>
+                {edu.title}
+            </a>;
+            if(count==0){
+                eduTag = <a data-toggle="collapse" data-parent="#accordion" href={`#${collapseClass}`} aria-expanded="true"     aria-controls={collapseClass}>
+                    {edu.title}
+                </a>;
+            }
+            count++;
             return(
                 <div className="panel panel-default" key={edu.title}>
                     <div className="panel-heading" role="tab" id="headingOne">
                         <h4 className="panel-title">
-                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                {edu.title}
-                            </a>
+                            {eduTag}
                         </h4>
                     </div>
-                    <div id="collapseOne" className="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div id={collapseClass} className={collapseDivClass} role="tabpanel" aria-labelledby={areaLabel}>
                         <div className="panel-body">
                             <div className="row">
                                 <div className="col-md-6">
